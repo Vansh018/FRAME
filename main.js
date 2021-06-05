f710f96c20575e16f57717c2d5f988d753305837
Vansh018 <75937381+Vansh018@users.noreply.github.com>
2021-06-05 17:36:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 
 
 function setup(){
-    canvas = createCanvas(1000,1000);
+    canvas = createCanvas(350,300);
     canvas.position(300, 350);
     video = createCapture(VIDEO);
     video.hide();
-    circle(300,95,20);
-    circle(500,95,20);
-    circle(500,300,20);
-    circle(300,300,20);
+    video.center();
+    
 }
 
 function draw(){
-    image(video, 300, 100, 200, 200);
+    image(video, 80, 50, 200, 200);
    
 }
 function take_snapshot()
